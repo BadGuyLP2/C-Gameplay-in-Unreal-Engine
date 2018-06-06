0a7a7d148fdc9d8f3f0ff570134dff32a6335eaf
DESKTOP-900KLP2\Daniel Stan <badguylp2@gmail.com>
2018-06-06 18:57:31 +0300

    C009 - Started adding cpp classes.

---
+++ Cpp_Gameplay_in_UE/Source/Cpp_Gameplay_in_UE/TankPlayerController.h
@@ -0,0 +1,22 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "Tank.h"
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerController.h"
+#include "TankPlayerController.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class CPP_GAMEPLAY_IN_UE_API ATankPlayerController : public APlayerController
+{
+	GENERATED_BODY()
+	
+public:
+	ATank * GetControllerTank() const;
+	
+	
+};
